9a4399abde6a78ddb2f0451170cf5475de91da35
tariqulseopro <tariqulseopro@gmail.com>
2025-04-26 22:07:16 +0600

    update name

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
 
-class ecommerce extends StatefulWidget {
-  const ecommerce({super.key});
-
-  @override
-  State<ecommerce> createState() => _ecommerceState();
-}
-
-class _ecommerceState extends State<ecommerce> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
